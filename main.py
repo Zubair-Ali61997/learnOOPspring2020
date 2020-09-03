ba86a1050182e58cb2f1eba9a10023968c5d4df0
Zubair-Ali61997 <68123141+Zubair-Ali61997@users.noreply.github.com>
2020-09-03 22:39:23 +0500

    Add files via upload
    lab_task

--- main.py
+++ main.py
@@ -1,53 +1,19 @@
-#Taking the range of number from the user to find even and odd using for loop
-startNum = int(input("Enter starting value: "))
-endNum = int(input("Enter end value: "))
-for eachNumber in range(startNum,endNum+1):
-    modeValue = eachNumber % 2
-    if modeValue == 0:
-        print(eachNumber, "is an even")
-    else:
-        print(eachNumber, "is an odd")
+#Table using while loop
 
-#Taking the range of number from the user to find even and odd using while loop
 
-# taking_a_value = int(input("Enter a value: "))
-startNum = int(input("Enter starting value: "))
-endNum = int(input("Enter end value: "))
+table = int(input("Enter a number: "))
+startNum = int(input("Enter a start number: "))
+endNum = int(input("Enter a end number: "))
+i = startNum
+while (i <= endNum):
+    print (i,"*",table,"=",i*table)
+    i=i+1
+print("#######################################################")
+#Table using for loop
 
-while (startNum >= endNum):
-    modeValue = endNum % 2
-    if modeValue == 0:
-        print(endNum, "is an even")
-    else:
-        print(endNum, "is an odd")
-    endNum=endNum+1
-
-#Finding totel number of odd and even in a range using for loop
-startNum = int(input("Enter starting value: "))
-endNum = int(input("Enter end value: "))
-evenNumber = 0
-oddNumber = 0
-for eachNumber in range (startNum, endNum+1):
-    modeValue = eachNumber % 2
-    if modeValue == 0:
-        evenNumber = evenNumber + 1
-    else:
-        oddNumber = oddNumber + 1
-print ("Total number of even number is = ",evenNumber)
-print ("Total number of odd number is = ",oddNumber)
-
-# Finding totel number of odd and even in a range using while loop
-startNum = int(input("Enter starting value: "))
-endNum = int(input("Enter end value: "))
-evenNumber = 0
-oddNumber = 0
-while (startNum >= endNum):
-    modeValue = endNum % 2
-    if modeValue== 0:
-        evenNumber = evenNumber + 1
-    else:
-        oddNumber = oddNumber + 1
-
-    endNum = endNum + 1
-print ("Total number of even number is = ",evenNumber)
-print ("Total number of odd number is = ",oddNumber)
+table = int(input("Enter a number: "))
+startNum = int(input("Enter a start number: "))
+endNUm = int(input("Enter a end number: "))
+for foreachnumber in range(startNum, endNum+1):
+    print (i,"*",table,"=",i*table)
+print("########################################################")
